0224095f3b90eb9c247f77be18e8765e97597246
hyunrak <hyunrak.ko@partner.so>
2018-05-24 20:23:50 +0900

    upload project

---
+++ CefSharpWinform/CefSharpWinform/ChromiumWinform.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CefSharp;
+using CefSharp.WinForms;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace CefSharpWinform
+{
+    public partial class ChromiumWinform: UserControl
+    {
+        public ChromiumWinform()
+        {
+            InitializeComponent();
+            InitializeChromium();
+        }
+
+        #region Chromium
+        public ChromiumWebBrowser chromeBrowser;
+
+        public void InitializeChromium()
+        {
+            var cefPath = Application.StartupPath + @"\";
+
+            CefSettings settings = new CefSettings();
+
+            // Copy from Wpf version
+            settings.LocalesDirPath = System.IO.Path.Combine(Application.StartupPath, "locales");
+            settings.Locale = CultureInfo.CurrentCulture.EnglishName;
+            settings.CachePath = System.IO.Path.Combine(cefPath, "cache");
+            settings.LogFile = System.IO.Path.Combine(cefPath, "CefLog.txt");
+            settings.LogSeverity = CefSharp.LogSeverity.Info;
+            settings.BrowserSubprocessPath = System.IO.Path.Combine(cefPath, "CefSharp.BrowserSubprocess.exe");
+            settings.SetOffScreenRenderingBestPerformanceArgs();
+
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser("www.google.com");            
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;            
+        }
+        #endregion
+
+        #region Method
+        public void ExecuteJavaScriptAsync(string _json, string scriptUrl = "about:blank", int startLine = 1)
+        {
+            chromeBrowser.GetBrowser().MainFrame.ExecuteJavaScriptAsync(_json, scriptUrl, startLine);
+        }
+        
+        public void RegisterJsObject(string name, object objectToBind, BindingOptions options = null)
+        {
+            chromeBrowser.RegisterJsObject(name, objectToBind);
+        }
+        #endregion
+
+        #region
+        private IContextMenuHandler _menuHandler;
+        public IContextMenuHandler MenuHandler
+        {
+            get { return _menuHandler; }
+            set
+            {
+                _menuHandler = value;
+            }
+        }
+
+        private string _url = string.Empty;
+        public string URL
+        {
+            get { return _url; }
+            set { _url = value; }
+        }
+        #endregion
+    }
+}
---
+++ HostingWinformCtrlFromWpf/HostingWinformCtrlFromWpf/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp;
+using CefSharpWinform;
+
+namespace HostingWinformCtrlFromWpf
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CefSharpWinform.ChromiumWinform uc = new CefSharpWinform.ChromiumWinform();
+            
+
+            //uc.MenuHandler = new CefCustomContextHandler();
+
+            uc.chromeBrowser = new CefSharp.WinForms.ChromiumWebBrowser("www.google.com");
+            uc.chromeBrowser.FrameLoadEnd += (sender, e) =>
+            {
+
+            };
+
+
+            WinformsHost.Child = uc;
+        }
+
+        /// <summary>
+        /// 웹페이지 컨텍스트 메뉴 제거
+        /// </summary>
+        public class CefCustomContextHandler : IContextMenuHandler
+        {
+            public void OnBeforeContextMenu(IWebBrowser browserControl, CefSharp.IBrowser browser, IFrame frame, IContextMenuParams parameters,
+                IMenuModel model)
+            {
+                model.Clear();
+            }
+
+            public bool OnContextMenuCommand(IWebBrowser browserControl, CefSharp.IBrowser browser, IFrame frame, IContextMenuParams parameters, CefMenuCommand commandId, CefEventFlags eventFlags)
+            {
+                return false;
+            }
+
+            public void OnContextMenuDismissed(IWebBrowser browserControl, CefSharp.IBrowser browser, IFrame frame)
+            {
+            }
+
+            public bool RunContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model, IRunContextMenuCallback callback)
+            {
+                return false;
+            }
+        }
+    }
+}
